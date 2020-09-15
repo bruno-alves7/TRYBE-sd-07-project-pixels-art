d9b9acb5249d93bf0c978674f2fcb156e97ae4fb
spawnred <bruno.spawnhell@gmail.com>
2020-09-15 18:26:08 -0300

    itens 4

---
+++ script.js
@@ -0,0 +1,21 @@
+let pixelBoard = document.getElementById("pixel-board");
+let num = 5;
+let pixel = [];
+let linha = [];
+let k = 0;
+
+
+for(let i = 0; i < num; i += 1){
+    linha[i] = document.createElement("div");
+    linha[i].className = "linha";
+    pixelBoard.appendChild(linha[i]);
+
+    for(let j = 0; j < num ; j += 1) {
+        pixel[k] = document.createElement("div")
+        pixel[k].className = "pixel";
+        linha[i].appendChild(pixel[k])
+        k += 1;
+    }
+}
+console.log(pixel);
+
