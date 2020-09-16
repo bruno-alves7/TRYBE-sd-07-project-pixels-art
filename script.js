dbe9e0bd3b7d9ee80f7dd766cb5378d32a62a407
spawnred <bruno.spawnhell@gmail.com>
2020-09-15 21:53:57 -0300

    erro item 7

--- script.js
+++ script.js
@@ -21,8 +21,16 @@ for(let i = 0; i < num; i += 1){
 let color = document.querySelectorAll(".color");
 for(let i = 0 ; i <color.length ; i += 1){
     color[i].addEventListener("click", function (event) {
-    let selected = document.querySelector(".selected")
-    selected.classList.remove("selected");
-    event.target.className += " selected"
-})
+        let selected = document.querySelector(".selected")
+        selected.classList.remove("selected");
+        event.target.className += " selected"
+    })
+}
+
+for(let i = 0; i < pixel.length; i += 1){
+    pixel[i].addEventListener("click", function (){
+        let selected = document.querySelector(".selected");
+        pixel[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor;
+        
+    })
 }
